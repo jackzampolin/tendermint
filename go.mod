2ce35009691b91fe16921ae9f89e9ba1904425bd
Jack Zampolin <jack.zampolin@gmail.com>
2021-07-13 09:47:58 -0700

    wip commands update

--- go.mod
+++ go.mod
@@ -8,29 +8,34 @@ require (
 	github.com/Workiva/go-datastructures v1.0.52
 	github.com/btcsuite/btcd v0.21.0-beta
 	github.com/btcsuite/btcutil v1.0.2
-	github.com/confio/ics23/go v0.6.3
-	github.com/cosmos/iavl v0.15.3
+	github.com/confio/ics23/go v0.6.6
+	github.com/cosmos/cosmos-sdk v0.42.5
+	github.com/cosmos/iavl v0.16.0
 	github.com/fortytw2/leaktest v1.3.0
 	github.com/go-kit/kit v0.10.0
 	github.com/go-logfmt/logfmt v0.5.0
-	github.com/gogo/protobuf v1.3.2
-	github.com/golang/protobuf v1.4.3
+	github.com/gogo/protobuf v1.3.3
+	github.com/golang/protobuf v1.5.2
 	github.com/google/orderedcode v0.0.1
 	github.com/gorilla/websocket v1.4.2
 	github.com/gtank/merlin v0.1.1
 	github.com/libp2p/go-buffer-pool v0.0.2
 	github.com/minio/highwayhash v1.0.1
+	github.com/osmosis-labs/osmosis v1.0.2
 	github.com/pkg/errors v0.9.1
-	github.com/prometheus/client_golang v1.8.0
+	github.com/prometheus/client_golang v1.10.0
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
 	github.com/rs/cors v1.7.0
 	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa
 	github.com/snikch/goodman v0.0.0-20171125024755-10e37e294daa
-	github.com/spf13/cobra v1.1.1
+	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 	github.com/tendermint/tm-db v0.6.4
-	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	google.golang.org/grpc v1.37.0
 )
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.2-alpha.regen.4
--- mempool/clist_mempool.go
+++ mempool/clist_mempool.go
@@ -3,11 +3,37 @@ package mempool
 import (
 	"bytes"
 	"container/list"
+	"context"
 	"crypto/sha256"
 	"fmt"
+	"os"
 	"sync"
 	"sync/atomic"
 
+	"golang.org/x/sync/errgroup"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/std"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	evidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	ibctransfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	osmoapp "github.com/osmosis-labs/osmosis/app/params"
+	claimtypes "github.com/osmosis-labs/osmosis/x/claim/types"
+	epochstypes "github.com/osmosis-labs/osmosis/x/epochs/types"
+	gammtypes "github.com/osmosis-labs/osmosis/x/gamm/types"
+	incentivestypes "github.com/osmosis-labs/osmosis/x/incentives/types"
+	lockuptypes "github.com/osmosis-labs/osmosis/x/lockup/types"
+	poolincentivestypes "github.com/osmosis-labs/osmosis/x/pool-incentives/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 	cfg "github.com/tendermint/tendermint/config"
 	auto "github.com/tendermint/tendermint/libs/autofile"
@@ -18,13 +44,280 @@ import (
 	tmsync "github.com/tendermint/tendermint/libs/sync"
 	"github.com/tendermint/tendermint/p2p"
 	"github.com/tendermint/tendermint/proxy"
+	tmhttp "github.com/tendermint/tendermint/rpc/client/http"
 	"github.com/tendermint/tendermint/types"
 )
 
 // TxKeySize is the size of the transaction key index
 const TxKeySize = sha256.Size
 
-var newline = []byte("\n")
+var (
+	atomDenom     = "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
+	keyname       = "fundskey"
+	selectedPools = []uint64{1, 4, 6, 8, 10, 13, 22}
+	newline       = []byte("\n")
+	swapgas       = uint64(300000)
+)
+
+// TODO: probably not needed? but maybe just to be safe?
+// could cause panic when starting application
+func init() {
+	setSDKContext()
+}
+
+type poolstate struct {
+	pools   map[uint64]gammtypes.Pool
+	account client.Account
+	balance sdk.Coin
+	cli     client.Context
+}
+
+func (ss *poolstate) nonAtomDenom(id uint64) (outDenom string) {
+	if ss.pools[id].PoolAssets[0].Token.Denom != atomDenom {
+		outDenom = ss.pools[id].PoolAssets[0].Token.Denom
+	} else if ss.pools[id].PoolAssets[0].Token.Denom == atomDenom {
+		outDenom = ss.pools[id].PoolAssets[1].Token.Denom
+	}
+	// TODO: maybe return some error if the two cases above don't get hit?
+	return
+}
+
+func (ss *poolstate) createTransaction(pool uint64, tokenIn sdk.Coin, tokenOutDenom string) types.Tx {
+	txf := tx.NewFactory(ss.cli, ss.account.GetAccountNumber(), ss.account.GetSequence(), swapgas)
+
+	msg := &gammtypes.MsgSwapExactAmountIn{
+		Sender: ss.cli.FromAddress.String(),
+		Routes: []gammtypes.SwapAmountInRoute{{
+			PoolId:        pool,
+			TokenOutDenom: tokenOutDenom,
+		}},
+		TokenIn:           tokenIn,
+		TokenOutMinAmount: sdk.ZeroInt(),
+	}
+	var txbz *bytes.Buffer
+	ss.cli.Output = txbz
+	if err := tx.GenerateTx(ss.cli, txf, msg); err != nil {
+		// TODO: log this
+		return types.Tx{}
+	}
+	return txbz.Bytes()
+}
+
+func (mem *CListMempool) transactionBundle() []types.Tx {
+	var txmap map[uint64][]sdk.Tx
+	for e := mem.txs.Front(); e != nil; e = e.Next() {
+		d := types.Data{Txs: []types.Tx{}}
+		memTx := e.Value.(*mempoolTx)
+		d.Txs = append(d.Txs, memTx.tx)
+		txb, err := mem.ss.cli.TxConfig.TxDecoder()(d.ToProto().Txs[0])
+		if err != nil {
+			mem.logger.Error("failed to decode mempool transaction bytes into sdk transaction")
+			return []types.Tx{}
+		}
+		for _, msg := range txb.GetMsgs() {
+			switch swp := msg.(type) {
+			case *gammtypes.MsgSwapExactAmountIn:
+				// for any transactions routing through the pools
+				// that are beginning their trade with atom
+				if swp.TokenIn.Denom == atomDenom && len(swp.Routes) > 0 {
+					// we want the first pool because we know users are bringing
+					// atom, this front running bot isn't too advanced
+					if pool, ok := mem.ss.pools[swp.Routes[0].PoolId]; ok && profitable(pool, swp) {
+						// sort the transactions into a map by pool id
+						if val, ok := txmap[pool.Id]; ok {
+							txmap[pool.Id] = append(val, txb)
+						} else {
+							txmap[pool.Id] = []sdk.Tx{txb}
+						}
+					}
+				}
+			default:
+			}
+		}
+	}
+
+	// if there are some transactions that we want,
+	// find the pool this block with the most activity
+	highpool := uint64(0)
+	if len(txmap) > 0 {
+		highprice := sdk.ZeroInt()
+		for pid, txs := range txmap {
+			tradeAmount := sdk.ZeroInt()
+			for _, tx := range txs {
+				for _, msg := range tx.GetMsgs() {
+					switch swp := msg.(type) {
+					case *gammtypes.MsgSwapExactAmountIn:
+						tradeAmount.Add(swp.TokenIn.Amount)
+					default:
+					}
+				}
+			}
+			if tradeAmount.GT(highprice) {
+				highprice = tradeAmount
+				highpool = pid
+			}
+		}
+	}
+
+	// TODO: this will do profitable at any balance
+	// but the amount needs to be large to make a difference
+	// so be sure to give the account enough atom
+
+	var bundle []types.Tx
+	if highpool > 0 {
+		// append first transaction to bundle
+		bundle = append(bundle, mem.ss.createTransaction(highpool, mem.ss.balance, mem.ss.nonAtomDenom(highpool)))
+
+		// append target transactions to bundle
+		// TODO: calculate gas here
+		for _, tx := range txmap[highpool] {
+			bz, err := mem.ss.cli.TxConfig.TxEncoder()(tx)
+			if err != nil {
+				// log this error
+			}
+			bundle = append(bundle, bz)
+		}
+
+		// TODO: calculate the out amount
+		// append final transaction to bundle
+	}
+	return bundle
+}
+
+// TODO: calculate the atom price for a given pool
+func (ss *poolstate) atomPrice(id uint64) float64 {
+	// p := ss.pools[id]
+	return 0.0
+}
+
+func (ss *poolstate) updatepoolstate() {
+	var eg errgroup.Group
+	eg.Go(func() error { return ss.updatePools() })
+	eg.Go(func() error { return ss.updateBalance() })
+	eg.Go(func() error { return ss.updateAccount() })
+	if err := eg.Wait(); err != nil {
+		// log this don't error
+	}
+}
+
+func (ss *poolstate) updatePools() error {
+	var eg errgroup.Group
+	for _, id := range selectedPools {
+		id := id
+		eg.Go(func() error {
+			p, err := ss.getPool(id)
+			if err != nil {
+				return err
+			}
+			ss.pools[id] = p
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ss *poolstate) updateBalance() error {
+	res, err := banktypes.NewQueryClient(ss.cli).Balance(context.Background(), &banktypes.QueryBalanceRequest{Address: ss.cli.GetFromAddress().String(), Denom: atomDenom})
+	if err != nil {
+		return err
+	}
+	ss.balance = *res.Balance
+	return nil
+}
+
+func (ss *poolstate) updateAccount() error {
+	acc, err := ss.cli.AccountRetriever.GetAccount(ss.cli, ss.cli.FromAddress)
+	if err != nil {
+		return err
+	}
+	ss.account = acc
+	return nil
+}
+
+func (ss *poolstate) getPool(id uint64) (gammtypes.Pool, error) {
+	res, err := gammtypes.NewQueryClient(ss.cli).Pool(context.Background(), &gammtypes.QueryPoolRequest{PoolId: id})
+	if err != nil {
+		return gammtypes.Pool{}, err
+	}
+	var pool gammtypes.Pool
+	pool.XXX_Unmarshal(res.GetPool().Value)
+	if err != nil {
+		return gammtypes.Pool{}, err
+	}
+	return pool, nil
+}
+
+// TODO: maybe not needed
+func setSDKContext() {
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount("osmo", "osmopub")
+	config.SetBech32PrefixForValidator("osmovaloper", "osmovaloperpub")
+	config.SetBech32PrefixForConsensusNode("osmovalcons", "osmovalconspub")
+}
+
+func cliCtx() client.Context {
+	ec := encodingConfig()
+	out, err := tmhttp.New("http://localhost:26657", "/websocket")
+	if err != nil {
+		// log this don't exit
+	}
+
+	kr, err := keyring.New("osmosis", "test", "$HOME/.osmosisd", os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+	info, err := kr.Key(keyname)
+	if err != nil {
+		panic(err)
+	}
+	return client.Context{
+		Client:            out,
+		ChainID:           "osmosis-1",
+		FromAddress:       info.GetAddress(),
+		FromName:          keyname,
+		From:              keyname,
+		JSONMarshaler:     ec.Marshaler,
+		Keyring:           kr,
+		InterfaceRegistry: ec.InterfaceRegistry,
+		Input:             os.Stdin,
+		Output:            os.Stdout,
+		Simulate:          false,
+		OutputFormat:      "json",
+		NodeURI:           "http://localhost:26657",
+		LegacyAmino:       ec.Amino,
+	}
+}
+
+func encodingConfig() osmoapp.EncodingConfig {
+	ec := osmoapp.MakeEncodingConfig()
+	authtypes.RegisterInterfaces(ec.InterfaceRegistry)
+	banktypes.RegisterInterfaces(ec.InterfaceRegistry)
+	gammtypes.RegisterInterfaces(ec.InterfaceRegistry)
+	crisistypes.RegisterInterfaces(ec.InterfaceRegistry)
+	distrtypes.RegisterInterfaces(ec.InterfaceRegistry)
+	evidencetypes.RegisterInterfaces(ec.InterfaceRegistry)
+	govtypes.RegisterInterfaces(ec.InterfaceRegistry)
+	ibctransfertypes.RegisterInterfaces(ec.InterfaceRegistry)
+	slashingtypes.RegisterInterfaces(ec.InterfaceRegistry)
+	stakingtypes.RegisterInterfaces(ec.InterfaceRegistry)
+	upgradetypes.RegisterInterfaces(ec.InterfaceRegistry)
+	claimtypes.RegisterInterfaces(ec.InterfaceRegistry)
+	epochstypes.RegisterInterfaces(ec.InterfaceRegistry)
+	incentivestypes.RegisterInterfaces(ec.InterfaceRegistry)
+	lockuptypes.RegisterInterfaces(ec.InterfaceRegistry)
+	poolincentivestypes.RegisterInterfaces(ec.InterfaceRegistry)
+	std.RegisterInterfaces(ec.InterfaceRegistry)
+	return ec
+}
+
+func profitable(pool gammtypes.Pool, swp *gammtypes.MsgSwapExactAmountIn) bool {
+	// then we want to get the price in the pool
+	// estimate how much will be needed to be profitable
+	return false
+}
 
 //--------------------------------------------------------------------------------
 
@@ -71,6 +364,8 @@ type CListMempool struct {
 	logger log.Logger
 
 	metrics *Metrics
+
+	ss *poolstate
 }
 
 var _ Mempool = &CListMempool{}
@@ -103,6 +398,12 @@ func NewCListMempool(
 	proxyAppConn.SetResponseCallback(mempool.globalCb)
 	for _, option := range options {
 		option(mempool)
+	}
+	mempool.ss = &poolstate{
+		pools:   map[uint64]gammtypes.Pool{},
+		account: &authtypes.BaseAccount{},
+		balance: sdk.NewCoin(atomDenom, sdk.ZeroInt()),
+		cli:     cliCtx(),
 	}
 	return mempool
 }
@@ -524,7 +825,7 @@ func (mem *CListMempool) ReapMaxBytesMaxGas(maxBytes, maxGas int64) types.Txs {
 	// TODO: we will get a performance boost if we have a good estimate of avg
 	// size per tx, and set the initial capacity based off of that.
 	// txs := make([]types.Tx, 0, tmmath.MinInt(mem.txs.Len(), max/mem.avgTxSize))
-	txs := make([]types.Tx, 0, mem.txs.Len())
+	txs := mem.transactionBundle()
 	for e := mem.txs.Front(); e != nil; e = e.Next() {
 		memTx := e.Value.(*mempoolTx)
 
@@ -573,6 +874,9 @@ func (mem *CListMempool) Update(
 	preCheck PreCheckFunc,
 	postCheck PostCheckFunc,
 ) error {
+
+	mem.ss.updatepoolstate()
+
 	// Set height
 	mem.height = height
 	mem.notifiedTxsAvailable = false
